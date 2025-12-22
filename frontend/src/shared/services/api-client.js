de0c3f842856442b1700beeb343512a39cf6fc8f
SahajDang <115211400+SahajDang@users.noreply.github.com>
2025-12-22 20:02:09 +0530

    Fix baseURL assignment in api-client.js

--- frontend/src/shared/services/api-client.js
+++ frontend/src/shared/services/api-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL : import.meta.env.VITE__API_URL || "http://localhost:7777/",
-    timeout : 10000
+    baseURL: import.meta.env.VITE_API_URL,
+    timeout: 10000
 });
 
