3e83af8e5e0059ce1cc0f111322abe3d309d703f
SahajDang <115211400+SahajDang@users.noreply.github.com>
2025-12-22 19:02:53 +0530

    Implement basic HTTP server in Server.java

---
+++ judge/java/Server.java
@@ -0,0 +1,24 @@
+package judge.java;
+
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        int port = Integer.parseInt(
+            System.getenv().getOrDefault("PORT", "8080")
+        );
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Judge service running";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+
+        server.start();
+        System.out.println("Judge running on port " + port);
+    }
+}
