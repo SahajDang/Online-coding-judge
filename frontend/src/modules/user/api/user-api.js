26e9d02ce54e44c048a26e67fe7e5bbd77f360eb
SahajDang <115211400+SahajDang@users.noreply.github.com>
2025-12-22 20:27:16 +0530

    Fix API endpoint paths in user-api.js

--- frontend/src/modules/user/api/user-api.js
+++ frontend/src/modules/user/api/user-api.js
@@ -1,11 +1,11 @@
-import { api } from "../../../shared/services/api-client"
+import { api } from "../../../shared/services/api-client";
 
 export const doLogin = async (userObject) => {
-    const response = await api.post('login', userObject);
+    const response = await api.post("/login", userObject);
     return response.data;
-}
+};
 
 export const doRegister = async (userObject) => {
-    const response = await api.post('register', userObject);
+    const response = await api.post("/register", userObject);
     return response.data;
-}
+};
